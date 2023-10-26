c890c225c5f12c528cc7ef68f90840dcf69a437d
hardpsycho <harddeveloper@outlook.com>
2023-10-26 06:09:54 +0300

    add support ts in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { ProgressPlugin } = require('webpack');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
         clean: true,
         filename: '[name].[contenthash].js',
@@ -13,5 +13,17 @@ module.exports = {
     plugins: [
         new ProgressPlugin(),
         new HtmlWebpackPlugin({template: './public/index.html'})
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+    },
 }
