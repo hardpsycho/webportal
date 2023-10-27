137a19ac2ec106d9a5401d7ecd023693e703d51f
hardpsycho <harddeveloper@outlook.com>
2023-10-27 13:13:54 +0300

    fixed trouble webpack absolute paths

--- config/webpack/builders/resolveBuilder.ts
+++ config/webpack/builders/resolveBuilder.ts
@@ -1,7 +1,26 @@
+import path from 'path'
+
 import { ResolveOptions } from 'webpack'
 
 export function resolveBuilder(): ResolveOptions {
     return {
-        extensions: ['.tsx', '.ts', '.js']
+        extensions: ['.tsx', '.ts', '.js'],
+        modules: [
+            path.resolve(__dirname, '../../..', 'src'),
+            path.resolve(__dirname, '../../..'),
+            'node_modules'
+        ],
+        alias: {
+            '@config': [path.resolve(__dirname, '../../..', 'config/')],
+            '@public': [path.resolve(__dirname, '../../..', 'public/')],
+            '@app': [path.resolve(__dirname, '../../..', 'src/app/')],
+            '@shared': [path.resolve(__dirname, '../../..', 'src/shared/')],
+            '@entities': [path.resolve(__dirname, '../../..', 'src/entities/')],
+            '@features': [path.resolve(__dirname, '../../..', 'src/features/')],
+            '@pages': [path.resolve(__dirname, '../../..', 'src/pages/')],
+            '@widgets': [path.resolve(__dirname, '../../..', 'src/widgets/')],
+            '@layout': [path.resolve(__dirname, '../../..', 'src/layout/')]
+        },
+        preferAbsolute: true
     }
 }
