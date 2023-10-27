f3b60b48593157d0cb1786f73986b8c093315a2b
hardpsycho <harddeveloper@outlook.com>
2023-10-27 15:16:32 +0300

    add template main and about page

---
+++ src/pages/aboutPage/ui/aboutPage.tsx
@@ -0,0 +1,9 @@
+import { type FC } from 'react'
+
+interface AboutPageProps {}
+
+const AboutPage: FC<AboutPageProps> = () => {
+    return <div>about page</div>
+}
+
+export default AboutPage
---
+++ src/pages/mainPage/ui/mainPage.tsx
@@ -0,0 +1,9 @@
+import { type FC } from 'react'
+
+interface MainProps {}
+
+const MainPage: FC<MainProps> = () => {
+    return <div>main Page</div>
+}
+
+export default MainPage
