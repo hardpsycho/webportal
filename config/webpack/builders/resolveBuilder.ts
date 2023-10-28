d374c55b90363575943ef01a61a9f5d69e50f19f
hardpsycho <harddeveloper@outlook.com>
2023-10-28 06:14:14 +0300

    add const root in resolveBuilder

--- config/webpack/builders/resolveBuilder.ts
+++ config/webpack/builders/resolveBuilder.ts
@@ -3,23 +3,21 @@ import path from 'path'
 import { ResolveOptions } from 'webpack'
 
 export function resolveBuilder(): ResolveOptions {
+    const rootFolder = path.resolve(__dirname, '../../..')
+
     return {
         extensions: ['.tsx', '.ts', '.js'],
-        modules: [
-            path.resolve(__dirname, '../../..', 'src'),
-            path.resolve(__dirname, '../../..'),
-            'node_modules'
-        ],
+        modules: [path.join(rootFolder, 'src'), path.join(rootFolder), 'node_modules'],
         alias: {
-            '@config': [path.resolve(__dirname, '../../..', 'config/')],
-            '@public': [path.resolve(__dirname, '../../..', 'public/')],
-            '@app': [path.resolve(__dirname, '../../..', 'src/app/')],
-            '@shared': [path.resolve(__dirname, '../../..', 'src/shared/')],
-            '@entities': [path.resolve(__dirname, '../../..', 'src/entities/')],
-            '@features': [path.resolve(__dirname, '../../..', 'src/features/')],
-            '@pages': [path.resolve(__dirname, '../../..', 'src/pages/')],
-            '@widgets': [path.resolve(__dirname, '../../..', 'src/widgets/')],
-            '@layout': [path.resolve(__dirname, '../../..', 'src/layout/')]
+            '@config': [path.join(rootFolder, 'config/')],
+            '@public': [path.join(rootFolder, 'public/')],
+            '@app': [path.join(rootFolder, 'src/app/')],
+            '@shared': [path.join(rootFolder, 'src/shared/')],
+            '@entities': [path.join(rootFolder, 'src/entities/')],
+            '@features': [path.join(rootFolder, 'src/features/')],
+            '@pages': [path.join(rootFolder, 'src/pages/')],
+            '@widgets': [path.join(rootFolder, 'src/widgets/')],
+            '@layouts': [path.join(rootFolder, 'src/layouts/')]
         },
         preferAbsolute: true
     }
