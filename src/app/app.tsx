0a97663367dd3295fbb7961beeac1868459b03e2
hardpsycho <harddeveloper@outlook.com>
2023-10-28 08:51:10 +0300

    update app & root for work with themes

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,11 +2,17 @@ import { type FC } from 'react'
 import { RouterProvider } from 'react-router-dom'
 
 import { router } from '@shared/config/router'
+import { ThemeProvider } from '@shared/theme/themeProvider'
+import './styles/main.scss'
 
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
-    return <RouterProvider router={router} />
+    return (
+        <ThemeProvider>
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    )
 }
 
 export { App }
--- src/layouts/root/ui/root.tsx
+++ src/layouts/root/ui/root.tsx
@@ -1,16 +1,21 @@
-import { Suspense, type FC } from 'react'
+import { Suspense, useContext, type FC } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+
+import { ThemeContext } from '@shared/theme/themeContext'
 
 interface RootProps {}
 
 const Root: FC<RootProps> = () => {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+
     return (
-        <div>
+        <div className={`app ${theme}`}>
             <header>
                 <nav>
                     <NavLink to={'/'}>Главная</NavLink>
                     <NavLink to={'/about'}>О нас</NavLink>
                 </nav>
+                <button onClick={toggleTheme}>сменить тему</button>
             </header>
             <main>
                 <Suspense fallback={<>Loading...</>}>
