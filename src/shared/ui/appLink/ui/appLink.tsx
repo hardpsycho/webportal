8425d90d02c9d6f3e62121c485dedc86c48c9d6a
hardpsycho <harddeveloper@outlook.com>
2023-11-01 07:48:48 +0300

    create appLink

---
+++ src/shared/ui/appLink/ui/appLink.tsx
@@ -0,0 +1,31 @@
+import { clsx } from 'clsx'
+import { ReactNode, type FC } from 'react'
+import { NavLink, NavLinkProps } from 'react-router-dom'
+
+import styles from './appLink.m.scss'
+
+export enum AppLinkVariant {
+    PRIMARY = 'primary',
+    INVERTED = 'inverted'
+}
+
+interface AppLinkProps extends NavLinkProps {
+    children: ReactNode
+    to: string
+    variant?: AppLinkVariant
+}
+
+const AppLink: FC<AppLinkProps> = ({
+    children,
+    to,
+    variant = AppLinkVariant.PRIMARY,
+    ...otherProps
+}) => {
+    return (
+        <NavLink {...otherProps} to={to} className={clsx(styles.appLink, styles[variant])}>
+            {children}
+        </NavLink>
+    )
+}
+
+export { AppLink }
