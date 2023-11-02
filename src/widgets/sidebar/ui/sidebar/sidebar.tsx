3a48e38766aed4efddd6e6592ecd060c8d5c04c2
hardpsycho <harddeveloper@outlook.com>
2023-11-02 18:02:50 +0300

    add sidebar

---
+++ src/widgets/sidebar/ui/sidebar/sidebar.tsx
@@ -0,0 +1,29 @@
+import { clsx } from 'clsx'
+import { useState, type FC } from 'react'
+
+import { Button, ButtonVariant } from '@shared/ui/button'
+import styles from './sidebar.m.scss'
+
+interface SidebarProps {}
+
+const Sidebar: FC<SidebarProps> = () => {
+    const [collapsed, setCollapsed] = useState(false)
+
+    const sidebarToggle = function () {
+        setCollapsed((prev) => !prev)
+    }
+
+    return (
+        <div className={clsx(styles.sidebar, { [styles.collapsed]: collapsed })}>
+            <Button onClick={sidebarToggle} theme={ButtonVariant.GHOST}>
+                {collapsed ? 'Открыть' : 'закрыть'}
+            </Button>
+            <div className={styles.switchers}>
+                {/* ThemeSwitcher */}
+                {/* LangSwitcher */}
+            </div>
+        </div>
+    )
+}
+
+export { Sidebar }
