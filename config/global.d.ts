18e8f966fbb87b54b0bf864a7f2d8c67e43aebf7
hardpsycho <harddeveloper@outlook.com>
2023-11-02 12:20:54 +0300

    update webpack cfg for svg

--- config/global.d.ts
+++ config/global.d.ts
@@ -2,3 +2,8 @@ declare module '*.module.css'
 declare module '*.module.scss'
 declare module '*.m.css'
 declare module '*.m.scss'
+declare module '*.svg' {
+    const svg: React.FC<React.SVGProps<SVGElement>>
+    export default svg
+}
+declare module '*.svg?url'
--- config/webpack/builders/moduleBuilder.ts
+++ config/webpack/builders/moduleBuilder.ts
@@ -34,7 +34,20 @@ export function moduleBuilder(configOption: ConfigOptions): ModuleOptions {
         ]
     }
 
+    const svgLoader: RuleSetRule = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: ['@svgr/webpack']
+    }
+
+    const svgLoaderInline: RuleSetRule = {
+        test: /\.svg$/i,
+        type: 'asset',
+        resourceQuery: /url/ // *.svg?url
+    }
+
     return {
-        rules: [tsLoaderRule, styleLoadersRule]
+        rules: [tsLoaderRule, styleLoadersRule, svgLoader, svgLoaderInline]
     }
 }
