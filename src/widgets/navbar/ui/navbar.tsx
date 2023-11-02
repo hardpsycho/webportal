6160cc35dc2e90de1d725b77e8d0f3748346ce46
hardpsycho <harddeveloper@outlook.com>
2023-11-02 12:22:40 +0300

    add themeSwitcher in Navbar

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -2,12 +2,14 @@ import { type FC } from 'react'
 
 import { AppLink, AppLinkVariant } from '@shared/ui/appLink'
 import styles from './navbar.m.scss'
+import { ThemeSwitcher } from '@features/themeSwitcher'
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
     return (
         <div className={styles.navbar}>
+            <ThemeSwitcher />
             <nav className={styles.links}>
                 <AppLink variant={AppLinkVariant.INVERTED} to={'/'}>
                     Главная
