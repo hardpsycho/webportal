fc5c157c85171726b30328620955a7d19224af2d
hardpsycho <harddeveloper@outlook.com>
2023-11-03 09:34:15 +0300

    add t in about page

--- src/pages/aboutPage/ui/aboutPage.tsx
+++ src/pages/aboutPage/ui/aboutPage.tsx
@@ -1,9 +1,12 @@
 import { type FC } from 'react'
+import { useTranslation } from 'react-i18next'
 
 interface AboutPageProps {}
 
 const AboutPage: FC<AboutPageProps> = () => {
-    return <div>about page</div>
+    const { t } = useTranslation('about')
+
+    return <div>{t('about-page')}</div>
 }
 
 export default AboutPage
