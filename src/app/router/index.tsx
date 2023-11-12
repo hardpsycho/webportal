a82f3323441288a796b7b7c61e113772b355f4f3
hardpsycho <harddeveloper@outlook.com>
2023-11-12 12:46:17 +0300

    add error element into router

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -4,11 +4,13 @@ import { Root } from '@layouts/root'
 import { AboutPage } from '@pages/aboutPage'
 import { MainPage } from '@pages/mainPage'
 import { NotFoundPage } from '@pages/notFoundPage'
+import { ErrorComponent } from '@shared/ui/errorComponent'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
+        errorElement: <ErrorComponent />,
         children: [
             {
                 path: '/',
