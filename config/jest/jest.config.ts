b317b838f74d8be2754fc7398ea0d62ca1798f55
hardpsycho <harddeveloper@outlook.com>
2023-11-14 15:42:34 +0300

    update jest configfor rtl

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -65,7 +65,9 @@ const config: Config = {
     // globalTeardown: undefined,
 
     // A set of global variables that need to be available in all test environments
-    // globals: {},
+    globals: {
+        WP_DEV: true
+    },
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
@@ -77,7 +79,23 @@ const config: Config = {
     moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {
+        // мок стилей
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        // мок файлов
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/config/jest/mocks/fileMock.ts',
+        // absolyute paths
+        '@app/(.*)': '<rootDir>/src/app/$1',
+        '@public/(.*)': '<rootDir>/public/$1',
+        '@shared/(.*)': '<rootDir>/src/shared/$1',
+        '@entities/(.*)': '<rootDir>/src/entities/$1',
+        '@features/(.*)': '<rootDir>/src/features/$1',
+        '@pages/(.*)': '<rootDir>/src/pages/$1',
+        '@widgets/(.*)': '<rootDir>/src/widgets/$1',
+        '@layouts/(.*)': '<rootDir>/src/layouts/$1',
+        '@config/(.*)': '<rootDir>/config/$1'
+    },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
@@ -89,7 +107,7 @@ const config: Config = {
     // notifyMode: "failure-change",
 
     // A preset that is used as a base for Jest's configuration
-    // preset: undefined,
+    preset: 'ts-jest',
 
     // Run tests from one or more projects
     // projects: undefined,
@@ -122,7 +140,7 @@ const config: Config = {
     // setupFiles: [],
 
     // A list of paths to modules that run some code to configure or set up the testing framework before each test
-    // setupFilesAfterEnv: [],
+    setupFilesAfterEnv: ['<rootDir>/config/jest/setupJest.ts'],
 
     // The number of seconds after which a test is considered as slow and reported as such in the results.
     // slowTestThreshold: 5,
@@ -140,7 +158,7 @@ const config: Config = {
     // testLocationInResults: false,
 
     // The glob patterns Jest uses to detect test files
-    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)']
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
@@ -160,10 +178,7 @@ const config: Config = {
     // transform: undefined,
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "/node_modules/",
-    //   "\\.pnp\\.[^\\/]+$"
-    // ],
+    transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$']
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
