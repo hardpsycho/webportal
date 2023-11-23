04b4b2a798c627d52ade15ad477e67151eb0d587
hardpsycho <harddeveloper@outlook.com>
2023-11-23 15:58:12 +0300

    add scss style for themeDecorator

--- src/shared/libs/storybook/themeDecorator.tsx
+++ src/shared/libs/storybook/themeDecorator.tsx
@@ -1,13 +1,15 @@
 import { StoryFn } from '@storybook/react'
+import { clsx } from 'clsx'
 
 import { ThemeVariant } from '@shared/theme/themeProvider'
 import '@shared/config/i18n/i18n'
 import '@app/styles/main.scss'
+import './storybookRoot.scss'
 
 const themeDecorator = (theme: ThemeVariant) => {
     const _themeDecorator = (Story: StoryFn) => {
         return (
-            <div className={theme}>
+            <div className={clsx(theme, 'hp-sb-root')}>
                 <Story />
             </div>
         )
