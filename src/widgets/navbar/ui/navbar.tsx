afb4eed0744e0b3e98c58266a23541cb34d64828
hardpsycho <harddeveloper@outlook.com>
2023-11-28 13:51:57 +0300

    add login button into navbar with modal

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,11 +1,30 @@
-import { type FC } from 'react'
+import { useState, type FC } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import styles from './navbar.m.scss'
+import { Button, ButtonVariant } from '@shared/ui/button'
+import { Modal } from '@shared/ui/modal'
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
-    return <div className={styles.navbar}></div>
+    const { t } = useTranslation()
+    const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
+
+    return (
+        <div className={styles.navbar}>
+            <Button
+                variant={ButtonVariant.OUTLINED}
+                className={styles.btn}
+                onClick={() => setIsLoginModalOpen(true)}
+            >
+                {t('login')}
+            </Button>
+            <Modal isOpen={isLoginModalOpen} onCLose={() => setIsLoginModalOpen(false)}>
+                &
+            </Modal>
+        </div>
+    )
 }
 
 export { Navbar }
