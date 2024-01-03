5afc52523eb08f5ff7ee41b32b5a0373416c5c96
hardpsycho <harddeveloper@outlook.com>
2024-01-04 02:52:44 +0300

    update root layout

--- src/layouts/root/ui/root.tsx
+++ src/layouts/root/ui/root.tsx
@@ -8,11 +8,18 @@ import { Sidebar } from '@widgets/sidebar'
 import styles from './root.m.scss'
 import { PageSpinner } from '@shared/ui/pageSpinner'
 import { ErrorBoundary } from '@shared/ui/errorBoundary'
+import { useInitialAuth } from '@features/initialAuth'
 
 interface RootProps {}
 
 const Root: FC<RootProps> = () => {
     const { theme } = useTheme()
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { isLoading } = useInitialAuth()
+
+    if (isLoading) {
+        return <PageSpinner />
+    }
 
     return (
         <div id='root' className={clsx('app', theme)}>
