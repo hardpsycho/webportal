7b021e3b4728a2c15e048cf44f26a97b17f9e809
hardpsycho <harddeveloper@outlook.com>
2024-01-04 02:52:36 +0300

    update navbar

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,15 +1,37 @@
 import { useState, type FC } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch } from '@reduxjs/toolkit'
 
 import styles from './navbar.m.scss'
 import { Button, ButtonVariant } from '@shared/ui/button'
-import { Modal } from '@shared/ui/modal'
+import { ModalLoginForm } from '@features/authByUsername'
+import { getSessionId, logoutSession } from '@entities/session'
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
     const { t } = useTranslation()
+    const sessionId = useSelector(getSessionId)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const dispath = useDispatch<Dispatch<any>>()
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
+
+    const onLogout = () => {
+        console.log('click')
+        setIsLoginModalOpen(false)
+        dispath(logoutSession(null))
+    }
+
+    if (sessionId) {
+        return (
+            <div className={styles.navbar}>
+                <Button variant={ButtonVariant.OUTLINED} className={styles.btn} onClick={onLogout}>
+                    {t('logout')}
+                </Button>
+            </div>
+        )
+    }
 
     return (
         <div className={styles.navbar}>
@@ -20,9 +42,11 @@ const Navbar: FC<NavbarProps> = () => {
             >
                 {t('login')}
             </Button>
-            <Modal isOpen={isLoginModalOpen} onCLose={() => setIsLoginModalOpen(false)}>
-                &
-            </Modal>
+
+            <ModalLoginForm
+                isLoginModalOpen={isLoginModalOpen}
+                onCLose={() => setIsLoginModalOpen(false)}
+            />
         </div>
     )
 }
