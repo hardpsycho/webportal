91c681b035667e13c0d78b2a0c4d4c1f10211620
hardpsycho <harddeveloper@outlook.com>
2024-01-04 03:01:37 +0300

    fix bugs

--- src/app/store/config/rootStateSchema.ts
+++ src/app/store/config/rootStateSchema.ts
@@ -1,9 +1,7 @@
 import { SessionState } from '@entities/session'
-import { UserState } from '@entities/user'
 import { LoginState } from '@features/authByUsername'
 
 export interface RootStateSchema {
-    userState: UserState
     loginState: LoginState
     sessionState: SessionState
 }
--- src/app/store/config/store.ts
+++ src/app/store/config/store.ts
@@ -1,13 +1,11 @@
 import { ReducersMapObject, configureStore } from '@reduxjs/toolkit'
 
 import { RootStateSchema } from './rootStateSchema'
-import { userReducer } from '@entities/user'
 import { loginReducer } from '@features/authByUsername/model/slice/loginSlice'
 import { sessionReducer } from '@entities/session'
 
 export function createReduxStore(initialState?: RootStateSchema) {
     const rootReducer: ReducersMapObject<RootStateSchema> = {
-        userState: userReducer,
         loginState: loginReducer,
         sessionState: sessionReducer
     }
