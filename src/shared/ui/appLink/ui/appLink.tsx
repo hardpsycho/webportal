342a8cf01dc9a39b76ab7db007d2dce78a88e4b2
hardpsycho <harddeveloper@outlook.com>
2024-01-14 09:32:33 +0300

    add memo to AppLink

--- src/shared/ui/appLink/ui/appLink.tsx
+++ src/shared/ui/appLink/ui/appLink.tsx
@@ -1,5 +1,5 @@
 import { clsx } from 'clsx'
-import { ReactNode, type FC } from 'react'
+import { ReactNode, type FC, memo } from 'react'
 import { NavLink, NavLinkProps } from 'react-router-dom'
 
 import styles from './appLink.m.scss'
@@ -15,22 +15,20 @@ interface AppLinkProps extends NavLinkProps {
     variant?: AppLinkVariant
 }
 
-const AppLink: FC<AppLinkProps> = ({
-    children,
-    to,
-    variant = AppLinkVariant.PRIMARY,
-    className,
-    ...otherProps
-}) => {
-    return (
-        <NavLink
-            {...otherProps}
-            to={to}
-            className={clsx(styles.appLink, styles[variant], className)}
-        >
-            {children}
-        </NavLink>
-    )
-}
+const AppLink: FC<AppLinkProps> = memo(
+    ({ children, to, variant = AppLinkVariant.PRIMARY, className, ...otherProps }) => {
+        return (
+            <NavLink
+                {...otherProps}
+                to={to}
+                className={clsx(styles.appLink, styles[variant], className)}
+            >
+                {children}
+            </NavLink>
+        )
+    }
+)
+
+AppLink.displayName = 'AppLink'
 
 export { AppLink }
