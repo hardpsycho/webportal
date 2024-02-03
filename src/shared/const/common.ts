6022f790923e06e853ed82d5e205e9fe1a3d4ca8
hardpsycho <harddeveloper@outlook.com>
2024-02-03 20:12:40 +0300

    new consts Currency, Coubtry, DataStatus

---
+++ src/shared/const/common.ts
@@ -0,0 +1,20 @@
+export enum Currency {
+    RUB = 'RUB',
+    EURO = 'EURO',
+    USD = 'USD'
+}
+
+export enum Country {
+    RUSSIA = 'RUSSIA',
+    BELARUS = 'BELARUS',
+    UKARAINE = 'UKARAINE',
+    ARMENIA = 'ARMENIA',
+    KAZAHSTAN = 'KAZAHSTAN'
+}
+
+export enum DataStatus {
+    INIT = 'init',
+    LOADING = 'lading',
+    ERROR = 'error',
+    SUCCESS = 'success'
+}
